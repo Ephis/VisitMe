4d5f1250ea59c5c926dc35902bb0cc13799e6ada
Nicolai Reigstad <nicolai887@gmail.com>
2015-11-04 22:34:39 +0100

    Initial commit - Entity og Migration sat op

---
+++ VisitMe2/VisitMe2/Models/Login.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace VisitMe2.Models
+{
+    public class Login
+    {
+        public int id { get; set; }
+        [Required]
+        public String username { get; set; }
+
+        public String email { get; set; }
+
+        [Required]
+        public String password { get; set; }
+
+
+    }
+}
