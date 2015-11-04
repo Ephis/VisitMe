676504fafabaa317e23f7c241edf678e01ecc1cd
Nicolai Reigstad <nicolai887@gmail.com>
2015-11-05 00:54:17 +0100

    IKKE TESTET
    Påbegyndt login implemention med API key.
    
    Guide: http://bitoftech.net/2014/06/01/token-based-authentication-asp-net-web-api-2-owin-asp-net-identity/

--- VisitMe2/VisitMe2/Global.asax.cs
+++ VisitMe2/VisitMe2/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace VisitMe2
@@ -11,7 +12,6 @@ namespace VisitMe2
     {
         protected void Application_Start()
         {
-            GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
 }
